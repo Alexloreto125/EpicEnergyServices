f7c466f24117bbe056d89737a8c137bebc0e351c
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-04-22 11:54:38 +0200

    ExceptionsHandler + ErrorDTO added

---
+++ src/main/java/BuildWeekTeam7/EpicEnergyServices/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package BuildWeekTeam7.EpicEnergyServices.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
