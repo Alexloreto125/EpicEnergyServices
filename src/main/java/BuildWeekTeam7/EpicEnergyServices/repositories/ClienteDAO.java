5da930969ac8914a021505a4bfb87282686a422d
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-04-22 11:55:43 +0100

    Creation of the CustomerDAO interface

---
+++ src/main/java/BuildWeekTeam7/EpicEnergyServices/repositories/ClienteDAO.java
@@ -0,0 +1,6 @@
+package BuildWeekTeam7.EpicEnergyServices.repositories;
+import BuildWeekTeam7.EpicEnergyServices.entities.Clienti;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteDAO extends JpaRepository<Clienti, String> {
+}
