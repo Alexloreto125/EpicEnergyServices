7df3cb18e1cad2ccbe1a171beff0a09c2918264c
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-24 15:07:43 +0200

    Aggiunta validazione controller email e fatture

--- src/main/java/BuildWeekTeam7/EpicEnergyServices/controllers/EmailController.java
+++ src/main/java/BuildWeekTeam7/EpicEnergyServices/controllers/EmailController.java
@@ -2,10 +2,13 @@ package BuildWeekTeam7.EpicEnergyServices.controllers;
 
 import BuildWeekTeam7.EpicEnergyServices.entities.Clienti;
 import BuildWeekTeam7.EpicEnergyServices.entities.User;
+import BuildWeekTeam7.EpicEnergyServices.exceptions.BadRequestException;
 import BuildWeekTeam7.EpicEnergyServices.mailgun.EmailDTO;
 import BuildWeekTeam7.EpicEnergyServices.mailgun.MailgunSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +23,8 @@ public class EmailController {
 
     @PostMapping ("/send")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public void sendEmail(@RequestBody EmailDTO payload){
+    public void sendEmail(@RequestBody @Validated EmailDTO payload,  BindingResult validation){
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
         Clienti recipient = new Clienti(payload.email());
         this.mailgunSender.sendEmail(recipient, payload.subject(), payload.text());
     }
