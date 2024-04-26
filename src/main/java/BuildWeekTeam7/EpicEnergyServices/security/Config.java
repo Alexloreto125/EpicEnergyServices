57f498f01d0bf90756764708e16e20dab22c54a2
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-26 10:30:17 +0200

    Aggiunta configurazione cors

--- src/main/java/BuildWeekTeam7/EpicEnergyServices/security/Config.java
+++ src/main/java/BuildWeekTeam7/EpicEnergyServices/security/Config.java
@@ -2,6 +2,7 @@ package BuildWeekTeam7.EpicEnergyServices.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ public class Config {
         httpSecurity.formLogin(http -> http.disable());
         httpSecurity.csrf(http -> http.disable());
         httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.cors(Customizer.withDefaults());
         httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
         return httpSecurity.build();
     }
